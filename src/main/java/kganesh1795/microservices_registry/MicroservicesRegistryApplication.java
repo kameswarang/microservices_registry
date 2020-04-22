78a6d038061adcc0c56a591e6ad7d4ff1ac62d3a
Kameswaran Ganesh <kameswaran1795@gmail.com>
2020-04-22 13:04:39 +0000

    fixed typos

--- src/main/java/kganesh1795/microservices_registry/MicroservicesRegistryApplication.java
+++ src/main/java/kganesh1795/microservices_registry/MicroservicesRegistryApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class MicroservicesRegistryApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceRegistryApplication.class, args);
+		SpringApplication.run(MicroservicesRegistryApplication.class, args);
 	}
-
 }
--- src/test/java/kganesh1795/microservices_registry/MicroservicesRegistryApplicationTests.java
+++ src/test/java/kganesh1795/microservices_registry/MicroservicesRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package kganesh1795.microservice_registry;
+package kganesh1795.microservices_registry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MicroserviceRegistryApplicationTests {
+public class MicroservicesRegistryApplicationTests {
 
 	@Test
 	public void contextLoads() {
